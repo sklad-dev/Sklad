b0cdce31cad2dd56f47670708dbe5a99295565de
MorgenGramlich <danila.sergeyev@gmail.com>
2024-12-25 18:46:22 -0600

    Store all files in .hodag folder

--- src/graph_storage.zig
+++ src/graph_storage.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
+const utils = @import("./utils.zig");
 const NodeStorage = @import("./node_storage.zig").NodeStorage;
 const ConnectionStorage = @import("./connection_storage.zig").ConnectionStorage;
+
+const DATABASE_STORAGE = ".hodag";
 
 pub const GraphStorage = struct {
     allocator: std.mem.Allocator,
@@ -8,10 +11,12 @@ pub const GraphStorage = struct {
     connection_storage: ConnectionStorage,
 
     pub fn init(allocator: std.mem.Allocator, max_node_memtable_size: u16, max_connection_memtable_size: u16) !GraphStorage {
+        try utils.make_dir_if_not_exists(DATABASE_STORAGE);
+
         return GraphStorage{
             .allocator = allocator,
-            .node_storage = try NodeStorage.init(allocator, "./nodes", max_node_memtable_size),
-            .connection_storage = try ConnectionStorage.init(allocator, "./connections", max_connection_memtable_size),
+            .node_storage = try NodeStorage.init(allocator, DATABASE_STORAGE ++ "/nodes", max_node_memtable_size),
+            .connection_storage = try ConnectionStorage.init(allocator, DATABASE_STORAGE ++ "/connections", max_connection_memtable_size),
         };
     }
 };
